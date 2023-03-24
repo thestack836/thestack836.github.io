27976d1b69a192baad8f8fd96721c4a6bb8fd966
KietTQ <KietTQ@coin98.finance>
2023-03-24 16:18:07 +0700

    fixs

--- ladder-run/application.5f2ba.js
+++ ladder-run/application.5f2ba.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.9367d.json';
+          this.settingsPath = 'src/settings.9f71d.json';
           this.showFPS = false;
         }
 
--- ladder-run/index.ebea8.js
+++ ladder-run/index.ebea8.js
@@ -1,4 +1,4 @@
-System.register(["./application.67b22.js"], function (_export, _context) {
+System.register(["./application.5f2ba.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.67b22.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application67b22Js) {
-      Application = _application67b22Js.Application;
+    setters: [function (_application5f2baJs) {
+      Application = _application5f2baJs.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
